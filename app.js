c229bb969a1f560c9ec5623fab89b4dca3c9e6cc
fahd-93 <fahdmajzoub13@gmail.com>
2019-02-05 14:13:04 +0100

    upload var & rout has been  added

--- app.js
+++ app.js
@@ -12,6 +12,11 @@ const storage = multer.diskStorage({
     }
 });
 
+// Init Upload
+const upload = multer({
+    storage: storage
+}).single('myImage');
+
 // Init app
 const app = express();
 
@@ -23,6 +28,19 @@ app.use(express.static('./public'));
 
 app.get('/', (req, res) => res.render('index'));
 
+app.post('/upload', (req, res) => {
+    upload(req, res, (err) => {
+        if (err) {
+            res.render('index', {
+                msg: err
+            });
+        } else {
+            console.log(req.file);
+            res.send('test');
+        }
+    })
+})
+
 const port = 4000;
 
-app.listen(port, () => console.log('server started on port ${port}'));
+app.listen(port, () => console.log(`Server started on port ${port}`));
